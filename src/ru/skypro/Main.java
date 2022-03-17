2f8f53eaa3a988e6504b0847044325593e9b3065
Sam Smith <romanmissilin@mail.ru>
2022-03-17 05:31:49 +0300

    Домашняя работа готова

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,43 @@
 package ru.skypro;
 
+import java.time.Year;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1 () {
+        int Number = 0;
+
+        while (Number <= 9){
+            Number++;
+            System.out.println(Number);
+            }
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2 () {
+        for (int Friday = 1; Friday <= 31; Friday++) {
+            if (Friday % 5 == 0) {
+                System.out.println("Сегодня пятница " + Friday + " число. Необходимо подготовить отчет.");
+                continue;
+            }
+            System.out.println("тчёт делать не нужно!");
+        }
+
+    }
+
+    public static void task3 () {
+        for (int Year = 1822; Year <= 2122; Year++) {
+            if (Year % 79 == 0) {
+                System.out.println(Year);
+            }
+        }
     }
 }
